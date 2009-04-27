0ae7e1d219c09ebaa17a22fa3d4fb712c36d82de
vsharma <vsharma@22fcb604-0a34-c361-3295-eba1aca837e0>
2009-04-27 06:17:39 +0000

    Look for protein common name the old way if there is no entry in the cache table.

--- YRC_COLLAB/branches/for_maccoss/src/org/yeastrc/www/compare/CommonNameLookupUtil.java
+++ YRC_COLLAB/branches/for_maccoss/src/org/yeastrc/www/compare/CommonNameLookupUtil.java
@@ -15,6 +15,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.yeastrc.ms.dao.DAOFactory;
+import org.yeastrc.nr_seq.NRProtein;
+import org.yeastrc.nr_seq.NRProteinFactory;
 
 /**
  * 
@@ -31,19 +33,20 @@ public class CommonNameLookupUtil {
         return instance;
     }
 
-    public CommonListing getCommonListing(int nrseqProteinId) throws SQLException {
+    public CommonListing getCommonListing(int nrseqProteinId) throws Exception {
         
         Connection conn = null;
         Statement stmt = null;
         ResultSet rs = null;
+        
+        CommonListing listing = new CommonListing();
+        listing.setNrseqProteinId(nrseqProteinId);
+        
         try {
             conn = DAOFactory.instance().getConnection();
             String sql = "SELECT name, description FROM nrseqProteinCache WHERE proteinID="+nrseqProteinId+" ORDER BY name ";
             stmt = conn.createStatement();
             rs = stmt.executeQuery(sql);
-            
-            CommonListing listing = new CommonListing();
-            listing.setNrseqProteinId(nrseqProteinId);
             
             List<CommonNameDescription> cndList = new ArrayList<CommonNameDescription>();
             
@@ -57,17 +60,27 @@ public class CommonNameLookupUtil {
             }
             
             listing.setCommonNameDescription(cndList);
-            return listing;
         }
         finally {
-            if(conn != null) try {conn.close();}
+            if(conn != null) try {conn.close(); conn = null;}
             catch (SQLException e) {}
-            if(stmt != null) try {stmt.close();}
+            if(stmt != null) try {stmt.close(); stmt = null;}
             catch (SQLException e) {}
-            if(rs != null) try {rs.close();}
+            if(rs != null) try {rs.close(); rs = null;}
             catch (SQLException e) {}
         }
         
+        // If we did not find anything in the cache table look for it the old way
+        if(listing.getCommonNameDescription().size() == 0) {
+          NRProteinFactory nrpf = NRProteinFactory.getInstance();
+          NRProtein nrseqProt = null;
+          nrseqProt = (NRProtein)(nrpf.getProtein(nrseqProteinId));
+          CommonNameDescription cnd = new CommonNameDescription();
+          cnd.setName(nrseqProt.getListing());
+          cnd.setDescription(nrseqProt.getDescription());
+        }
+        
+        return listing;
     }
     
     public List<CommonListing> getCommonListings(List<Integer> nrseqProteinIds) throws SQLException {
