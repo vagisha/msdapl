b6e473f4ba5edd04ce8a4fde93222ac53e6a0406
vsharma <vsharma@22fcb604-0a34-c361-3295-eba1aca837e0>
2009-11-12 08:19:09 +0000

    If NrProteins's getListing method returns multiple comma-separated names, they are split into individual ProteinNameDescription instances.

--- YRC_COLLAB/branches/for_maccoss/src/org/yeastrc/www/compare/CommonNameLookupUtil.java
+++ YRC_COLLAB/branches/for_maccoss/src/org/yeastrc/www/compare/CommonNameLookupUtil.java
@@ -75,11 +75,15 @@ public class CommonNameLookupUtil {
           NRProteinFactory nrpf = NRProteinFactory.getInstance();
           NRProtein nrseqProt = null;
           nrseqProt = (NRProtein)(nrpf.getProtein(nrseqProteinId));
-          ProteinNameDescription cnd = new ProteinNameDescription();
-          cnd.setName(nrseqProt.getListing());
-          cnd.setDescription(nrseqProt.getDescription());
+          
           List<ProteinNameDescription> list = new ArrayList<ProteinNameDescription>();
-          list.add(cnd);
+          String[] names = nrseqProt.getListing().split(",");
+          for(String name: names) {
+              ProteinNameDescription cnd = new ProteinNameDescription();
+              cnd.setName(name.trim());
+              cnd.setDescription(nrseqProt.getDescription());
+              list.add(cnd);
+          }
           listing.setNameAndDescription(list);
         }
         
