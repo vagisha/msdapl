db53197e53bedaebc4034388878106533d2bec86
vsharma <vsharma@22fcb604-0a34-c361-3295-eba1aca837e0>
2010-04-21 04:19:47 +0000

    Extracted out javascript code from viewProteinSetComparison.jsp and viewProteinGroupSetComparison.jsp

---
+++ YRC_COLLAB/branches/tpp_support/WebRoot/js/comparison.js
@@ -0,0 +1,220 @@
+// ---------------------------------------------------------------------------------------
+// AJAX DEFAULTS
+// ---------------------------------------------------------------------------------------
+  $.ajaxSetup({
+  	type: 'POST',
+  	//timeout: 5000,
+  	dataType: 'html',
+  	error: function(xhr) {
+  			
+  				var statusCode = xhr.status;
+		  		// status code returned if user is not logged in
+		  		// reloading this page will redirect to the login page
+		  		if(statusCode == 303)
+ 					window.location.reload();
+ 				
+ 				// otherwise just display an alert
+ 				else {
+ 					alert("Request Failed: "+statusCode+"\n"+xhr.statusText);
+ 				}
+  			}
+  });
+  
+  $.blockUI.defaults.message = '<b>Loading...</b>'; 
+  $.blockUI.defaults.css.padding = 20;
+  $.blockUI.defaults.fadeIn = 0;
+  $.blockUI.defaults.fadeOut = 0;
+  $().ajaxStop($.unblockUI);
+   
+
+// ---------------------------------------------------------------------------------------
+// PAGE RESULTS
+// ---------------------------------------------------------------------------------------
+function pageResults(pageNum) {
+  	$("input#pageNum").val(pageNum);
+  	$("input#download").val("false");
+  	$("input#goEnrichment").val("false");
+  	$("input#cluster").val("false");
+  	//alert("setting to "+pageNum+" value set to: "+$("input#pageNum").val());
+  	$("form[name='proteinSetComparisonForm']").submit();
+}
+
+// ---------------------------------------------------------------------------------------
+// SORT RESULTS
+// ---------------------------------------------------------------------------------------
+function sortResults(sortBy, sortOrder) {
+  	$("input#pageNum").val(1);
+  	$("input#download").val("false");
+  	$("input#goEnrichment").val("false");
+  	$("input#cluster").val("false");
+  	$("input#sortBy").val(sortBy);
+  	$("input#sortOrder").val(sortOrder);
+  	$("form[name='proteinSetComparisonForm']").attr('target', '');
+  	$("form[name='proteinSetComparisonForm']").submit();
+}
+
+// ---------------------------------------------------------------------------------------
+// UPDATE RESULTS
+// ---------------------------------------------------------------------------------------
+function updateResults() {
+  	$("input#pageNum").val(1);
+  	$("input#download").val("false");
+  	$("input#goEnrichment").val("false");
+  	$("input#cluster").val("false");
+  	$("form[name='proteinSetComparisonForm']").attr('target', '');
+  	$("form[name='proteinSetComparisonForm']").submit();
+}
+
+// ---------------------------------------------------------------------------------------
+// CLUSTER RESULTS
+// ---------------------------------------------------------------------------------------
+function clusterResults() {
+  	$("input#pageNum").val(1);
+  	$("input#download").val("false");
+  	$("input#goEnrichment").val("false");
+  	$("input#cluster").val("true");
+  	$("form[name='proteinSetComparisonForm']").submit();
+}
+
+// ---------------------------------------------------------------------------------------
+// DOWNLOAD RESULTS
+// ---------------------------------------------------------------------------------------
+function downloadResults() {
+  	$("input#download").val("true");
+  	$("input#goEnrichment").val("false");
+  	$("input#cluster").val("false");
+  	$("form[name='proteinSetComparisonForm']").attr('target', '_blank');
+  	$("form[name='proteinSetComparisonForm']").submit();
+}
+
+// ---------------------------------------------------------------------------------------
+// GENE ONTOLOGY ENRICHMENT
+// ---------------------------------------------------------------------------------------
+function doGoEnrichmentAnalysis() {
+	$("input#download").val("false");
+	$("input#cluster").val("false");
+	$("input#goEnrichment").val("true");
+	if(!validateGoEnrichmentForm())
+    	return false;
+	$("form[name='proteinSetComparisonForm']").submit();
+}
+
+function validateGoEnrichmentForm() {
+	// fieldValue is a Form Plugin method that can be invoked to find the 
+    // current value of a field 
+    value = $('form input[@name=goEnrichmentPVal]').fieldValue();
+    valid = validateFloat(value, "P-Value", 0.0, 1.0);
+    if(!valid)	return false;
+    
+    return true;
+}
+
+function validateFloat(value, fieldName, min, max) {
+	var floatVal = parseFloat(value);
+	var valid = true;
+	if(isNaN(floatVal))						valid = false;
+	if(valid && floatVal < min)			valid = false;
+	if(max && (valid && floatVal > max))	valid = false;
+	if(!valid) {
+		if(max) alert("Value for "+fieldName+" should be between "+min+" and "+max);
+		else	alert("Value for "+fieldName+" should be >= "+min);
+	}
+	return valid;
+}
+// ---------------------------------------------------------------------------------------
+// TOGGLE AND, OR, NOT FILTERS
+// ---------------------------------------------------------------------------------------
+function toggleAndSelect(dsIndex, red, green, blue) {
+	
+	var id = "AND_"+dsIndex+"_select";
+	var value = $("input#"+id).val();
+	if(value == "true") {
+		$("input#"+id).val("false");	
+		$("td#AND_"+dsIndex+"_td").css("background-color", "#FFFFFF");
+	}
+	else {
+		var color = "rgb("+red+","+green+","+blue+")";
+		$("input#"+id).val("true");	
+		$("td#AND_"+dsIndex+"_td").css("background-color", color);
+	}
+}
+function toggleOrSelect(dsIndex, red, green, blue) {
+	var id = "OR_"+dsIndex+"_select";
+	var value = $("input#"+id).val();
+	if(value == "true") {
+		$("input#"+id).val("false");	
+		$("td#OR_"+dsIndex+"_td").css("background-color", "#FFFFFF");
+	}
+	else {
+		var color = "rgb("+red+","+green+","+blue+")";
+		$("input#"+id).val("true");	
+		$("td#OR_"+dsIndex+"_td").css("background-color", color);
+	}
+}
+function toggleNotSelect(dsIndex, red, green, blue) {
+	var id = "NOT_"+dsIndex+"_select";
+	var value = $("input#"+id).val();
+	if(value == "true") {
+		$("input#"+id).val("false");	
+		$("td#NOT_"+dsIndex+"_td").css("background-color", "#FFFFFF");
+	}
+	else {
+		var color = "rgb("+red+","+green+","+blue+")";
+		$("input#"+id).val("true");	
+		$("td#NOT_"+dsIndex+"_td").css("background-color", color);
+	}
+}
+function toggleXorSelect(dsIndex, red, green, blue) {
+	var id = "XOR_"+dsIndex+"_select";
+	var value = $("input#"+id).val();
+	if(value == "true") {
+		$("input#"+id).val("false");	
+		$("td#XOR_"+dsIndex+"_td").css("background-color", "#FFFFFF");
+	}
+	else {
+		var color = "rgb("+red+","+green+","+blue+")";
+		$("input#"+id).val("true");	
+		$("td#XOR_"+dsIndex+"_td").css("background-color", color);
+	}
+}
+
+// ---------------------------------------------------------------------------------------
+// TOGGLE FULL PROTEIN NAMES
+// ---------------------------------------------------------------------------------------
+function toggleFullNames() {
+	if($("#full_names").text() == "[Full Names]") {
+		$("#full_names").text("[Short Names]");
+		$(".full_name").show();
+		$(".short_name").hide();
+	}
+	else if($("#full_names").text() == "[Short Names]") {
+		$("#full_names").text("[Full Names]");
+		$(".full_name").hide();
+		$(".short_name").show();
+	}
+}
+
+// ---------------------------------------------------------------------------------------
+// TOGGLE FULL PROTEIN DESCRIPTIONS
+// ---------------------------------------------------------------------------------------
+function toggleFullDescriptions() {
+	if($("#full_descriptions").text() == "[Full Descriptions]") {
+		$("#full_descriptions").text("[Short Descriptions]");
+		$(".full_description").show();
+		$(".short_description").hide();
+	}
+	else if($("#full_descriptions").text() == "[Short Descriptions]") {
+		$("#full_descriptions").text("[Full Descriptions]");
+		$(".full_description").hide();
+		$(".short_description").show();
+	}
+}
+
+function showAllDescriptionsForProtein(nrseqId) {
+	$("#short_desc_"+nrseqId).hide();
+	$("#full_desc_"+nrseqId).show();
+}
+function hideAllDescriptionsForProtein(nrseqId) {
+	$("#short_desc_"+nrseqId).show();
+	$("#full_desc_"+nrseqId).hide();
+}
---
+++ YRC_COLLAB/branches/tpp_support/WebRoot/pages/internal/compare/comparisonFunctions.jsp
@@ -0,0 +1,92 @@
+<script src="<yrcwww:link path='js/comparison.js'/>"></script>
+<script>
+
+$(document).ready(function() {
+	
+	// make the table sortable
+   	makeSortable();
+    
+   $("#compare_results_pager1").attr('width', "95%").attr('align', 'center');
+   $("#compare_results_pager2").attr('width', "95%").attr('align', 'center');
+   
+    $("#compare_results").each(function() {
+   		var $table = $(this);
+   		$table.attr('width', "95%");
+   		$table.attr('align', 'center');
+   		$('.prot_descr', $table).css("font-size", "8pt");
+   		
+   		<%for(int i = 0; i < comparison.getDatasetCount(); i++) {
+   			String color = DatasetColor.get(i).R+", "+DatasetColor.get(i).G+","+DatasetColor.get(i).B;
+   		%>
+   			$("td.prot-found[id="+<%=String.valueOf(i)%>+"]", $table).css('background-color', "rgb(<%=color%>)");
+   		<%}%>
+   		
+   		$('td.prot-parsim', $table).css('color', '#FFFFFF').css('font-weight', 'bold');
+   	});
+   	
+ });
+ 
+ // ---------------------------------------------------------------------------------------
+// MAKE TABLE SORTABLE
+// ---------------------------------------------------------------------------------------
+function makeSortable() {
+
+   $(".sortable_table").each(function() {
+   		var $table = $(this);
+   		$table.attr('width', "100%");
+   		$table.attr('align', 'center');
+   		//$('tbody > tr:odd', $table).addClass("tr_odd");
+   		//$('tbody > tr:even', $table).addClass("tr_even");
+   		
+   		$('th', $table).each(function() {
+   		
+   				if($(this).is('.sortable')) {
+      					
+      				$(this).click(function() {
+						var sortBy = $(this).attr('id');
+						// sorting direction
+						var sortOrder = "<%=SORT_ORDER.ASC.name()%>";
+						// is the column already sorted?
+						if ($(this).is('.sorted-asc')) {
+		          			sortOrder = "<%=SORT_ORDER.DESC.name()%>";
+		        		}
+		        		else if ($(this).is('.sorted-desc')) {
+		          			sortOrder = "<%=SORT_ORDER.ASC.name()%>";
+		        		}
+		        		// do we have a default sorting order?
+		        		else if ($(this).is('.def-sorted-desc')) { 
+		          			sortOrder = "<%=SORT_ORDER.DESC.name()%>";
+		        		}
+		        		else if ($(this).is('.def-sorted-asc')) {
+		          			sortOrder = "<%=SORT_ORDER.ASC.name()%>";
+		        		}
+	        			sortResults(sortBy, sortOrder);
+      			});
+      		}
+      	});
+   });
+}
+ 
+ 
+// ---------------------------------------------------------------------------------------
+// SETUP THE PEPTIDES TABLE
+// ---------------------------------------------------------------------------------------
+function  setupPeptidesTable(table){
+		var $table = $(table);
+   		$table.attr('width', "60%");
+   		$table.attr('align', 'center');
+   		$table.css("margin", "5 5 5 5");
+   		
+   		<%for(int i = 0; i < comparison.getDatasetCount(); i++) {
+   			String color = DatasetColor.get(i).R+", "+DatasetColor.get(i).G+","+DatasetColor.get(i).B;
+   		%>
+   			$("td.pept-found[id="+<%=String.valueOf(i)%>+"]", $table).css('background-color', "rgb(<%=color%>)").css('color', "rgb(<%=color%>)");
+   		<%}%>
+   		
+   		$('td.pept-unique', $table).css('color', '#FFFFFF').css('font-weight', 'bold');
+   		makeSortableTable($table);
+   		
+   		
+}
+
+</script>
