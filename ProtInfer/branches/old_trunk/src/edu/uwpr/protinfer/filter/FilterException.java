e67f1ee90ceba7bd222c43f2b2480b9d0fb34157
vsharma <vsharma@22fcb604-0a34-c361-3295-eba1aca837e0>
2010-05-21 22:47:34 +0000

    deleting old trunk

--- ProtInfer/branches/old_trunk/src/edu/uwpr/protinfer/filter/FilterException.java
+++
@@ -1,8 +0,0 @@
-package edu.uwpr.protinfer.filter;
-
-public class FilterException extends Exception {
-
-    public FilterException(String message) {
-        super(message);
-    }
-}
--- ProtInfer/branches/old_trunk/src/edu/uwpr/protinfer/infer/ProteinInferrer.java
+++
@@ -1,9 +0,0 @@
-package edu.uwpr.protinfer.infer;
-
-import java.util.List;
-
-
-public interface ProteinInferrer {
-
-    public <S extends SpectrumMatch, T extends PeptideSpectrumMatch<S>> List<InferredProtein<S>> inferProteins(List<T> psmList);
-}
