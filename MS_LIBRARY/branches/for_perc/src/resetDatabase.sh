df95831129187be3767dab7f2102e1e5bd87cefa
vsharma <vsharma@22fcb604-0a34-c361-3295-eba1aca837e0>
2010-05-21 22:59:21 +0000

    deleting obsolete branch

--- MS_LIBRARY/branches/for_perc/src/resetDatabase.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/sh
-
-/bin/bash /Users/silmaril/WORK/UW/MS_LIBRARY/for_perc/schema/truncateTables.sh
-#/usr/local/mysql/bin/mysql -uroot -p314159 < /Users/silmaril/WORK/UW/MS_LIBRARY/for_perc/schema/msDatabase\ SQL_test.sql;
-# /usr/local/mysql/bin/mysql -uroot msData_test <  /Users/vagisha/WORK/MS_LIBRARY/new_lib/schema/msDigestionEnzyme.sql;
