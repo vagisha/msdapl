240d4b07628e514615bd9736ba5ec327538b4c75
silmaril <silmaril@22fcb604-0a34-c361-3295-eba1aca837e0>
2010-10-17 07:56:43 +0000

    copying MSDaPl code from YRC_COLLAB branch

---
+++ MSDaPl/trunk/tpp_support/src/org/yeastrc/experiment/SearchAnalysis.java
@@ -0,0 +1,87 @@
+/**
+ * SearchAnalysis.java
+ * @author Vagisha Sharma
+ * Apr 2, 2009
+ * @version 1.0
+ */
+package org.yeastrc.experiment;
+
+import java.sql.Date;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.yeastrc.ms.domain.analysis.MsSearchAnalysis;
+import org.yeastrc.ms.domain.search.Program;
+
+
+/**
+ * 
+ */
+public class SearchAnalysis implements MsSearchAnalysis {
+
+    
+    private final MsSearchAnalysis analysis;
+    private String analysisName;
+    private List<AnalysisFile> files;
+    
+    private static final Pattern tppVersionPattern = Pattern.compile("TPP\\s+(v\\d+\\.\\d+)");
+    
+    public SearchAnalysis(MsSearchAnalysis analysis) {
+        this.analysis = analysis;
+    }
+    
+    public void setFiles(List<AnalysisFile> files) {
+        this.files = files;
+    }
+    
+    public List<AnalysisFile> getFiles() {
+        return files;
+    }
+    
+    @Override
+    public Program getAnalysisProgram() {
+        return analysis.getAnalysisProgram();
+    }
+
+    @Override
+    public String getAnalysisProgramVersion() {
+        return analysis.getAnalysisProgramVersion();
+    }
+    
+    public String getAnalysisProgramVersionShort() {
+    	String version = getAnalysisProgramVersion();
+    	
+    	if(analysis.getAnalysisProgram() == Program.PEPTIDE_PROPHET) {
+    		Matcher m = tppVersionPattern.matcher(version);
+    		if(m.find()) {
+    			version = m.group(1);
+    		}
+    	}
+    	return version;
+    }
+
+    @Override
+    public int getId() {
+        return analysis.getId();
+    }
+
+    @Override
+    public Date getUploadDate() {
+        return analysis.getUploadDate();
+    }
+
+    public String getAnalysisName() {
+        return analysisName;
+    }
+
+    public void setAnalysisName(String analysisName) {
+        this.analysisName = analysisName;
+    }
+
+    @Override
+    public void setId(int analysisId) {
+        throw new UnsupportedOperationException();
+    }
+
+}
---
+++ MSDaPl/trunk/tpp_support/src/org/yeastrc/www/util/RoundingUtils.java
@@ -0,0 +1,48 @@
+/**
+ * RoundingUtils.java
+ * @author Vagisha Sharma
+ * Mar 2, 2010
+ * @version 1.0
+ */
+package org.yeastrc.www.util;
+
+import java.math.BigDecimal;
+
+/**
+ * 
+ */
+public class RoundingUtils {
+
+	
+	private static RoundingUtils instance = null;
+	
+	private RoundingUtils() {}
+	
+	public static RoundingUtils getInstance() {
+		if(instance == null) {
+			instance = new RoundingUtils();
+		}
+		return instance;
+	}
+	
+	public double roundOne(BigDecimal number) {
+        return roundOne(number.doubleValue());
+    }
+    public double roundOne(double num) {
+        return Math.round(num*10.0)/10.0;
+    }
+    
+	public double roundTwo(BigDecimal number) {
+        return roundTwo(number.doubleValue());
+    }
+    public double roundTwo(double num) {
+        return Math.round(num*100.0)/100.0;
+    }
+    
+    public double roundFour(BigDecimal number) {
+        return roundTwo(number.doubleValue());
+    }
+    public double roundFour(double num) {
+        return Math.round(num*10000.0)/10000.0;
+    }
+}
