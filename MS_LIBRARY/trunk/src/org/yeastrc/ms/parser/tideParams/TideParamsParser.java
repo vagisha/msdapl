8376e5bb62382cd3e4434c51e32596cd489b0dbd
vsharma <vsharma@22fcb604-0a34-c361-3295-eba1aca837e0>
2011-01-19 23:07:17 +0000

    Updated TideParamParser -- Each Tide parameter is now in a separate header line.  But the fasta file name still has to be read from the CommandLineIndex header.

--- MS_LIBRARY/trunk/src/org/yeastrc/ms/parser/tideParams/TideParamsParser.java
+++ MS_LIBRARY/trunk/src/org/yeastrc/ms/parser/tideParams/TideParamsParser.java
@@ -88,8 +88,8 @@ public class TideParamsParser implements SearchParamsDataProvider {
 	public void parseParams(String remoteServer, String paramFileDir) throws DataProviderException {
 		
     	this.remoteServer = remoteServer;
-    	List<Param> params = getSearchParams(paramFileDir);
-    	parseParams(params);
+    	paramList = getSearchParams(paramFileDir);
+    	parseParams(paramList);
 		
 	}
 
@@ -97,7 +97,8 @@ public class TideParamsParser implements SearchParamsDataProvider {
 
     	Map<String, String> headerMap = new HashMap<String, String>();
 
-    	for(String fileName: getSqtFiles(new File(paramFileDir))) {
+    	List<String> sqtFiles = getSqtFiles(new File(paramFileDir));
+    	for(String fileName: sqtFiles) {
 
     		String filePath = paramFileDir+File.separator+fileName;
 
@@ -107,57 +108,71 @@ public class TideParamsParser implements SearchParamsDataProvider {
     		SQTRunSearchIn search = provider.getSearchHeader();
 
     		// These are the headers we are interested in
+    		// H    StaticMod       C=57.0215
     		// H	CommandLineIndex	--digestion=full-digest........
     		// H	CommandLineSearch	--proteins=.......
     		// H	CommandLineResults	--results_file
+    		// H 	MassWindow 3
+    		// H 	TopMatches 5
+    		// H 	ShowAllProteins 0
+    		// H 	ShowMods 0
+    		// H 	MinMass 600
+    		// H 	MaxMass 1000
+    		// H 	MinLength 6
+    		// H 	MaxLength 50
+    		// H 	Enzyme trypsin
+    		// H 	FullDigestion 1
+    		// H 	MissedCleavages 0
+    		// H 	MonoisotopicPrecursor 0
     		for(SQTHeaderItem header: search.getHeaders()) {
 
-    			if(header.getName().equalsIgnoreCase("CommandLineIndex") ||
-    					header.getName().equalsIgnoreCase("CommandLineSearch") ||
-    					header.getName().equalsIgnoreCase("CommandLineResults")) {
+    			// We are no longer interested in these headers for getting the Tide parameters
+        		// Each parameter is now present in its own header line
+        		if(header.getName().equalsIgnoreCase("CommandLineSearch") ||
+        		   header.getName().equalsIgnoreCase("CommandLineResults") ||
+        		   header.getName().equalsIgnoreCase("SQTGenerator")) {
+        			continue;
+        		}
 
-    				// We want to make sure that all SQT file in an experiment directory have the 
-    				// same header values
-    				String oldValue = headerMap.get(header.getName());
-    				if(oldValue != null) {
-    					if(!(oldValue.equalsIgnoreCase(header.getValue()))) {
-    						throw new DataProviderException("!!!SQT header mismatch");
-    					}
-    					else
-    						headerMap.put(header.getName(), header.getValue());
-    				}
-    			}
+        		// We want to make sure that all SQT files in an experiment directory have the 
+        		// same header values
+        		String oldValue = headerMap.get(header.getName());
+        		if(oldValue != null) {
+        			if(!(oldValue.equalsIgnoreCase(header.getValue()))) {
+        				throw new DataProviderException("!!!SQT header mismatch. Old value: "+oldValue+"; New value: "+header.getValue());
+        			}
+        		}
+        		else
+        			headerMap.put(header.getName(), header.getValue());
     		}
     		provider.close(); // close the file
-    		
-    	}
-
-    	// TEMPORARY
-    	// If we did not find the required headers in the SQT file add the values that were used
-    	// to process YRC peptide atlas data
-    	if(!headerMap.containsKey("CommandLineIndex")) {
-    		String value = "./index --digestion=full-digest --enzyme=none "
-    			+"--fasta=/net/pr/vol3/nobackups/tide_yrc/dbase/SGDyeast.20101221.fasta "+
-    			"--max_mass=5000 --min_mass=600 "+
-    			"--mods_spec=C+57.021464 "+
-    			"--monoisotopic_precursor=true --missed_cleavages=false --max_length=50 --min_length=6";
-
-    		headerMap.put("CommandLineIndex", value);
     	}
 
     	List<Param> params = new ArrayList<Param>();
-    	for(String paramsString: headerMap.values()) {
-    		String[] opts = paramsString.split("\\s+");
-    		for(String opt: opts) {
-    			String[] optTokens = opt.split("=");
-    			if(!(optTokens.length == 2)) {
-    				String name = optTokens[0];
-    				name.replaceFirst("-*", "");
-    				ParamBean param = new ParamBean(name, optTokens[1]);
-    				params.add(param);
+    	for(String paramName: headerMap.keySet()) {
+    		
+    		// We only need to get the fasta file name from this header
+    		if(paramName.equalsIgnoreCase("CommandLineIndex")) {
+    			String cmdLine = headerMap.get(paramName);
+    			String[] opts = cmdLine.split("\\s+");
+    			for(String opt: opts) {
+    				String[] optTokens = opt.split("=");
+    				if(optTokens.length == 2) {
+    					String name = optTokens[0];
+    					name = name.replaceFirst("-*", "");
+    					if(name.equalsIgnoreCase("fasta")) {
+    						ParamBean param = new ParamBean(name, optTokens[1]);
+    						params.add(param);
+    					}
+    				}
     			}
     		}
+    		else {
+    			ParamBean param = new ParamBean(paramName, headerMap.get(paramName));
+    			params.add(param);
+    		}
     	}
+    	
     	return params;
     }
     
@@ -193,73 +208,6 @@ public class TideParamsParser implements SearchParamsDataProvider {
     	
     	for(Param param: params) {
     		
-    		/* FLAGS used by the program - index
-    		-fasta (Input FASTA file) type: string default: ""
-			-peptides (File of peptides to create) type: string default: ""
-			-proteins (File of raw proteins to create, as raw_proteins.proto)
-						type: string default: ""
-			-digestion (Digestion completeness. May be full-digest or partial-digest)
-						type: string default: "full-digest"
-			-enzyme (Digestion enzyme. May be none, trypsin, chymotrypsin, elastase,
-						clostripain, cyanogen-bromide, idosobenzoate, proline-endopeptidase,
-						staph-protease, modified-chymotrypsin, elastase-trypsin-chymotrypsin,
-						aspn.) type: string default: "none"
-			-max_length (Peptide length inclusion threshold) type: int32 default: 50
-			-max_mass (Peptide mass inclusion threshold) type: double default: 7200
-			-min_length (Peptide length inclusion threshold) type: int32 default: 6
-			-min_mass (Peptide mass inclusion threshold) type: double default: 200
-			-missed_cleavages (Allow missed cleavages in enzymatic digestion)
-						type: bool default: false
-			-mods_spec (Expression for static modifications to include. Specify a
-						comma-separated list of the form --mods=C+57.0,...) type: string
-						default: ""
-			-mods_table (Modification specification filename. May be given instead of
-			--mods_spec.) type: string default: ""
-			-monoisotopic_precursor (Use monoisotopic precursor masses rather than
-						average masses for residues.) type: bool default: false
-    		 */
-    		
-    		/* FLAGS used by the program - search
-    		-mass_window (Precursor mass tolerance in Daltons) type: double default: 3
-			-peptides (File of unfragmented peptides, as peptides.proto) type: string
-						default: ""
-			-proteins (File of proteins corresponding to peptides, as
-						raw_proteins.proto) type: string default: ""
-			-results (Results format. Can be text or protobuf.) type: string
-						default: "text"
-			-results_file (Results output file) type: string default: "results.tideres"
-			-spectra (Spectrum input file) type: string default: ""
-			-top_matches (Number of matches to report for each spectrum) type: int32
-						default: 5
-    		 */
-    		
-    		/*
-    		-aux_locations (File of auxiliary locations corresponding to peptides in
-						the results) type: string default: ""
-			-match_fields (A comma delimited set of fields to show for a matching
-						peptide in the order listed. Available options are: xcorr,sequence)
-						type: string default: "xcorr,sequence"
-			-out_filename (Name of the output file to generate. An extension will be
-						added based on the out_format.) type: string default: ""
-			-out_format (The output format to be generated. Can be text, pep.xml or
-						sqt. Default is text.) type: string default: "text"
-			-protein_fields (A comma delimited set of fields to show for a protein
-						associated with a matching peptide. Available options are:
-						protein_name,pos, aa_before,aa_after) type: string
-						default: "protein_name,pos,aa_before,aa_after"
-			-proteins (File of proteins corresponding to peptides, as
-						raw_proteins.proto) type: string default: ""
-			-results_file (Results file generated via Search, as results.proto)
-						type: string default: ""
-			-show_all_proteins (Display all the proteins the peptide was found in.)
-						type: bool default: false
-			-show_mods (Display modifications in the peptide sequence.) type: bool
-						default: false
-			-spectra (Spectrum input file) type: string default: ""
-			-spectrum_fields (A comma delimited set of fields to show for an
-						experimental spectrum in the order listed. Available options are:
-						spectrum_num,mz,charge) type: string default: "spectrum_num,mz,charge"
-    		 */
     		
     		// fasta file used for search
     		if(param.getParamName().equalsIgnoreCase("fasta")) {
@@ -270,63 +218,182 @@ public class TideParamsParser implements SearchParamsDataProvider {
     		}
     		
     		// enzyme
-            else if (param.getParamName().equalsIgnoreCase("enzyme")) {
-            	Enzyme enz = new Enzyme();
-                enz.setName(param.getParamName());
-                this.enzyme = enz;
+            else if (param.getParamName().equalsIgnoreCase("Enzyme")) {
+            	if(!param.getParamValue().equalsIgnoreCase("none")) {
+            		Enzyme enz = new Enzyme();
+            		enz.setName(param.getParamValue());
+            		this.enzyme = enz;
+            	}
             }
     		
     		// static modifications
-    		if(param.getParamName().equalsIgnoreCase("mods_spec")) {
+    		if(param.getParamName().equalsIgnoreCase("StaticMod")) {
     			getStaticResidueMods(param);
     		}
     		
-    		// TODO Don't know the command-line option for dynamic modifications
-    		
-    		// are all protein matches for a peptide printed
-    		if(param.getParamName().equalsIgnoreCase("-show_all_proteins")) {
-    			this.printAllProteinMatches = Boolean.parseBoolean(param.getParamValue());
+    		// dynamic modifications
+    		if(param.getParamName().equalsIgnoreCase("DiffMod")) {
+    			getDynamicResidueMods(param);
     		}
     		
+    		// are all protein matches for a peptide printed
+    		if(param.getParamName().equalsIgnoreCase("ShowAllProteins")) {
+    			int val = 0;
+    			try {
+    				val = Integer.parseInt(param.getParamValue());
+    				this.printAllProteinMatches = Boolean.parseBoolean(param.getParamValue());
+    			}
+    			catch (NumberFormatException e) {
+    				throw new DataProviderException("Error parsing ShowAllProteins parameter", e);
+    			}
+    			this.printAllProteinMatches = val == 1 ? true : false;
+    		}
     	}
     }
 
 
 	private void getStaticResidueMods(Param param) throws DataProviderException {
 		
-		// mods_spec=C+57.021464
+		// StaticMod       C=57.0215
 		List<ResidueModification> modifications = parseModifications(param);
 		staticResidueModifications.addAll(modifications);
 	}
 	
+	private void getDynamicResidueMods(Param param) throws DataProviderException {
+		
+		// DiffMod SYT=+80.000
+		List<ResidueModification> modifications = parseModifications(param);
+		dynamicResidueModifications.addAll(modifications);
+	}
+	
 	private List<ResidueModification> parseModifications(Param param) throws DataProviderException {
 		
+		if(param.getParamValue() == null || param.getParamValue().trim().length() == 0)
+			return new ArrayList<ResidueModification>(0);
+		
 		List<ResidueModification> modifications = new ArrayList<ResidueModification>();
-		// Example: mods_spec=C+57.021464
-		// paramName = mods_spec; paramValue=C+57.021464
+		// Example: StaticMod       C=57.0215
+		// paramName = StaticMod; paramValue = C=57.0215
+		// OR
+		// DiffMod SYT=+80.000
+		// paramName = DiffMod; paramValue = SYT=+80.000
 		String[] mods = param.getParamValue().split(",");
 		for(String modStr: mods) {
-			int idx = modStr.indexOf("+");
-			if(idx == -1)
-				idx = modStr.indexOf("-");
+			int idx = modStr.indexOf("=");
 			
 			if(idx == -1) {
 				throw new DataProviderException("Cannot parse modification string: "+modStr);
 			}
 			
-			char modResidue = modStr.charAt(0);
-	        BigDecimal modMass = null;
+			BigDecimal modMass = null;
 	        try {modMass = new BigDecimal(modStr.substring(idx+1));}
 	        catch(NumberFormatException e) {throw new DataProviderException("Error parsing modification mass: "+modStr);}
-
-	        if (modMass.doubleValue() != 0.0) {
-	            ResidueModification mod = new ResidueModification();
-	            mod.setModificationMass(modMass);
-	            mod.setModifiedResidue(modResidue);
-	            modifications.add(mod);
-	        }
+			
+	        if(modMass.doubleValue() == 0.0)
+	        	continue;
+	        
+			for(int j = 0; j < idx; j++) {
+				char modResidue = modStr.charAt(j);
+				
+				ResidueModification mod = new ResidueModification();
+				mod.setModificationMass(modMass);
+				mod.setModifiedResidue(modResidue);
+				modifications.add(mod);
+			}
+	           
 		}
 		return modifications;
 	}
+	
+	
+	public static void main(String[] args) throws DataProviderException {
+		String dirPath = "/Users/silmaril/WORK/UW/MSDaPl_data/YRC_PEPTIDE_ATLAS/1174";
+		TideParamsParser parser = new TideParamsParser();
+		parser.parseParams("remote", dirPath);
+		
+		List<Param> params = parser.getParamList();
+		for(Param param: params) {
+			System.out.println(param.getParamName()+"\t"+param.getParamValue());
+		}
+		System.out.println(parser.getSearchDatabase().getDatabaseFileName());
+		System.out.println(parser.getSearchEnzyme());
+		System.out.println(parser.getSearchProgram());
+		System.out.println("Enzyme used: "+parser.isEnzymeUsedForSearch());
+		System.out.println("HAS all Protein matches: "+parser.printAllProteinMatches());
+		System.out.println(parser.getStaticResidueMods().size());
+		for(MsResidueModificationIn mod: parser.getStaticResidueMods())
+			System.out.println("STATIC "+mod.getModifiedResidue()+"; "+mod.getModificationMass());
+		
+		for(MsResidueModificationIn mod: parser.getDynamicResidueMods())
+			System.out.println("DYNAMIC "+mod.getModifiedResidue()+"; "+mod.getModificationMass());
+	}
+	
+	/* FLAGS used by the program - index
+	-fasta (Input FASTA file) type: string default: ""
+	-peptides (File of peptides to create) type: string default: ""
+	-proteins (File of raw proteins to create, as raw_proteins.proto)
+				type: string default: ""
+	-digestion (Digestion completeness. May be full-digest or partial-digest)
+				type: string default: "full-digest"
+	-enzyme (Digestion enzyme. May be none, trypsin, chymotrypsin, elastase,
+				clostripain, cyanogen-bromide, idosobenzoate, proline-endopeptidase,
+				staph-protease, modified-chymotrypsin, elastase-trypsin-chymotrypsin,
+				aspn.) type: string default: "none"
+	-max_length (Peptide length inclusion threshold) type: int32 default: 50
+	-max_mass (Peptide mass inclusion threshold) type: double default: 7200
+	-min_length (Peptide length inclusion threshold) type: int32 default: 6
+	-min_mass (Peptide mass inclusion threshold) type: double default: 200
+	-missed_cleavages (Allow missed cleavages in enzymatic digestion)
+				type: bool default: false
+	-mods_spec (Expression for static modifications to include. Specify a
+				comma-separated list of the form --mods=C+57.0,...) type: string
+				default: ""
+	-mods_table (Modification specification filename. May be given instead of
+	--mods_spec.) type: string default: ""
+	-monoisotopic_precursor (Use monoisotopic precursor masses rather than
+				average masses for residues.) type: bool default: false
+	 */
+	
+	/* FLAGS used by the program - search
+	-mass_window (Precursor mass tolerance in Daltons) type: double default: 3
+	-peptides (File of unfragmented peptides, as peptides.proto) type: string
+				default: ""
+	-proteins (File of proteins corresponding to peptides, as
+				raw_proteins.proto) type: string default: ""
+	-results (Results format. Can be text or protobuf.) type: string
+				default: "text"
+	-results_file (Results output file) type: string default: "results.tideres"
+	-spectra (Spectrum input file) type: string default: ""
+	-top_matches (Number of matches to report for each spectrum) type: int32
+				default: 5
+	 */
+	
+	/* FLAGS used by program - results
+	-aux_locations (File of auxiliary locations corresponding to peptides in
+				the results) type: string default: ""
+	-match_fields (A comma delimited set of fields to show for a matching
+				peptide in the order listed. Available options are: xcorr,sequence)
+				type: string default: "xcorr,sequence"
+	-out_filename (Name of the output file to generate. An extension will be
+				added based on the out_format.) type: string default: ""
+	-out_format (The output format to be generated. Can be text, pep.xml or
+				sqt. Default is text.) type: string default: "text"
+	-protein_fields (A comma delimited set of fields to show for a protein
+				associated with a matching peptide. Available options are:
+				protein_name,pos, aa_before,aa_after) type: string
+				default: "protein_name,pos,aa_before,aa_after"
+	-proteins (File of proteins corresponding to peptides, as
+				raw_proteins.proto) type: string default: ""
+	-results_file (Results file generated via Search, as results.proto)
+				type: string default: ""
+	-show_all_proteins (Display all the proteins the peptide was found in.)
+				type: bool default: false
+	-show_mods (Display modifications in the peptide sequence.) type: bool
+				default: false
+	-spectra (Spectrum input file) type: string default: ""
+	-spectrum_fields (A comma delimited set of fields to show for an
+				experimental spectrum in the order listed. Available options are:
+				spectrum_num,mz,charge) type: string default: "spectrum_num,mz,charge"
+	 */
     
 }
