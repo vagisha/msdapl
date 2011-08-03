426bcdb10c0586da5dde00cf01e0564219d2b05c
vsharma <vsharma@22fcb604-0a34-c361-3295-eba1aca837e0>
2011-08-03 00:21:01 +0000

    Every modification reported in the mzIdentML document has to be associated with a reference from the unimod controlled vocacubulary for protein modifications (http://www.unimod.org/unimod_help.html). Did the following to get a unimod reference for our modifications: 1. Added xjc generated classes for the Unimod schema. 2. Added unimod.xml in the resources directory 3. Added parser for unimod.xml and a lookup class that tries to find a unimod entry for a given residue and delta mass (+/- tolerance).

---
+++ MS_LIBRARY/trunk/src/org/yeastrc/ms/parser/unimod/UnimodRepository.java
@@ -0,0 +1,91 @@
+/**
+ * 
+ */
+package org.yeastrc.ms.parser.unimod;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import javax.xml.bind.JAXBException;
+
+import org.yeastrc.ms.parser.unimod.jaxb.ModT;
+import org.yeastrc.ms.parser.unimod.jaxb.ModificationsT;
+import org.yeastrc.ms.parser.unimod.jaxb.SpecificityT;
+import org.yeastrc.ms.parser.unimod.jaxb.UnimodT;
+
+/**
+ * UnimodRepository.java
+ * @author Vagisha Sharma
+ * Aug 2, 2011
+ * 
+ */
+public class UnimodRepository {
+
+	private List<ModT> modsList;
+	
+	public void initialize() throws UnimodRepositoryException {
+		
+		UnimodParser parser = new UnimodParser();
+		UnimodT unimod;
+		
+		try {
+			unimod = parser.read();
+		} catch (FileNotFoundException e) {
+			throw new UnimodRepositoryException("unimod.xml file was not found", e);
+		} catch (JAXBException e) {
+			throw new UnimodRepositoryException("Error reading unimod.xml file", e);
+		}
+		
+		ModificationsT mods = unimod.getModifications();
+		this.modsList = mods.getMod();
+	}
+	
+	public ModT getModification(char modifiedResidue, double deltaMonoMass, double tolerance, boolean isMonoMass) throws UnimodRepositoryException {
+		
+		if(modsList == null) {
+			throw new UnimodRepositoryException("Repository has not been initizlized");
+		}
+		
+		boolean matchChar = false;
+		
+		String modResStr = String.valueOf(modifiedResidue);
+		
+		for(ModT mod: modsList) {
+			
+			List<SpecificityT> specifities = mod.getSpecificity();
+			for(SpecificityT specificity: specifities) {
+				
+				if(specificity.getSite().equalsIgnoreCase(modResStr)) {
+					matchChar = true;
+				}
+						
+			}
+			
+			if(!matchChar) 
+				continue;
+			
+			double mass;
+			if(isMonoMass) {
+				mass = mod.getDelta().getMonoMass();
+			}
+			else {
+				mass = mod.getDelta().getAvgeMass();
+			}
+			
+			if((mass - tolerance) < deltaMonoMass && (mass + tolerance) > deltaMonoMass) {
+				return mod;
+			}
+		}
+		
+		return null;
+	}
+	
+	
+	public static void main(String[] args) throws UnimodRepositoryException {
+		
+		UnimodRepository rep = new UnimodRepository();
+		rep.initialize();
+		
+	}
+	
+}
