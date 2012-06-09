74029c02425af30a243c98de68a367b12eab5bee
vagisha <vagisha@gmail.com>
2012-06-09 16:44:29 +0000

    Added method to get user with a given email address -- this is for use with the job upload web-service.

--- MSDaPl/trunk/src/org/yeastrc/www/user/UserUtils.java
+++ MSDaPl/trunk/src/org/yeastrc/www/user/UserUtils.java
@@ -89,6 +89,74 @@ public class UserUtils {
 
 		return theUser;
 	}
+	
+	/**
+	 * Get a populated User object corresponding to a email address.
+	 * @param email The email address to test
+	 * @return The User object corresponding to that username.
+	 * @throws NoSuchUserException if that username does not exist.
+	 * @throws SQLException if a database error was encountered.
+	 */
+	public static User getUserWithEmail(String email) throws NoSuchUserException, SQLException {
+		// The User to return
+		User theUser;
+		
+		// Make sure the email isn't null
+		if (email == null) { throw new NoSuchUserException("got null for email in UserUtils.getUser"); }
+
+		// Get our connection to the database.
+		Connection conn = DBConnectionManager.getConnection("yrc");
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		try {
+			stmt = conn.prepareStatement("SELECT researcherID FROM tblResearchers WHERE researcherEmail = ?");
+			stmt.setString(1, email);
+
+			rs = stmt.executeQuery();
+		
+			// No rows returned.
+			if( !rs.next() ) {
+				throw new NoSuchUserException("email not found.");
+			}
+
+			theUser = new User();
+
+			try {
+				theUser.load(rs.getInt("researcherID"));
+			} catch(InvalidIDException e) {
+				throw new NoSuchUserException("Somehow, we got an invalid ID (" + rs.getInt("researcherID") + ") after we got the ID from the username...  This can't be good.");
+			}
+			
+			rs.close();
+			rs = null;
+			
+			stmt.close();
+			stmt = null;
+			
+			conn.close();
+			conn = null;
+		}
+		finally {
+
+			// Always make sure result sets and statements are closed,
+			// and the connection is returned to the pool
+			if (rs != null) {
+				try { rs.close(); } catch (SQLException e) { ; }
+				rs = null;
+			}
+			if (stmt != null) {
+				try { stmt.close(); } catch (SQLException e) { ; }
+				stmt = null;
+			}
+			if (conn != null) {
+				try { conn.close(); } catch (SQLException e) { ; }
+				conn = null;
+			}
+		}
+
+		return theUser;
+	}
 
 	/**
 	 * Get a populated User object from the request passed in.
