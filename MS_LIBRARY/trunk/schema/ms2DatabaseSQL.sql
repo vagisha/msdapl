06c92df0a9fc274ea6e95fe26c791b7006aca56c
djaschob <djaschob@uw.edu>
2014-08-07 22:51:44 +0000

    MS_LIBRARY - update qc plots

--- MS_LIBRARY/trunk/schema/ms2DatabaseSQL.sql
+++ MS_LIBRARY/trunk/schema/ms2DatabaseSQL.sql
@@ -756,20 +756,8 @@ ALTER TABLE BaristaProtein ADD INDEX(proteinGroupID);
 
 
 #####################################################################
-# Plot Tables
+# Plot Table
 #####################################################################
-
-CREATE TABLE mz_scan_count_plot_data (
-  experiment_id int(10) unsigned NOT NULL,
-  plot_data varchar(16000) NOT NULL,
-  scan_count int(10) unsigned NOT NULL,
-  create_time_in_seconds int(10) unsigned NOT NULL,
-  data_version int(10) unsigned NOT NULL,
-  PRIMARY KEY (experiment_id)
-);
-
-
-
 
 CREATE TABLE qc_plot_data (
   experiment_id int(10) unsigned NOT NULL,
